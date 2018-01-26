d967c86468567f37df1d8d2c7fb282bc9ff221a3
shadow091087 <ionut_androne_mail87@yahoo.com>
2018-01-27 00:41:23 +0200

    Add files via upload

---
+++ multilingual/template/front/footer.php
@@ -0,0 +1,20 @@
+
+		
+		</div>
+	</section>
+</div>
+<footer>
+	<div id="footer">
+		<div id="footer-left">
+			<p>&copy; <a href="/" title="Web Design Tutorials">SSDTutorials</a> <!-- date --></p>
+		</div>
+		<div id="footer-right">
+			<!-- navigation 3 -->
+		</div>
+	</div>
+</footer>
+</div>
+<script src="/js/jquery-1.6.4.min.js"></script>
+<script src="/js/core.js"></script>
+</body>
+</html>
